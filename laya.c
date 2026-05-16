746363ed0390c603f74d07508b174111140429c0
klayareddy2004 <klayareddy2004@gmail.com>
2026-05-16 11:45:07 +0530

    added the laya file feature (#14)

---
+++ laya.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//write 2 add numbers
+int main()
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("The sum of %d and %d is %d",a,b,a+b);
+    return 0;
+}
