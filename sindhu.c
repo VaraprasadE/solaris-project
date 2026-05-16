3ef1c70301e287e7eb93c011735c6430d547eaf0
jsindhuja09 <jsindhuja09@gmail.com>
2026-05-16 11:35:08 +0530

    added the sindhu name description (#15)

---
+++ sindhu.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+//explain about sindhu
+int main()
+{
+    printf("Sindhu is a river in the Indian subcontinent. It is one of the longest rivers in the world, flowing through China, India, and Pakistan. The river has great historical and cultural significance, as it was the cradle of the ancient Indus Valley Civilization. The Sindhu River is also known for its rich biodiversity and is an important water source for millions of people living along its banks.");
+    return 0;
+}
