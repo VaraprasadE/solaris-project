547dd710fa473eb94ca387fb4b5f8f004168ac04
TejaswiniThirunagari <thirunagaritejaswini525@gmail.com>
2026-05-16 10:31:46 +0530

    Add galaxystar program

---
+++ galaxystar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const char *stars[] = {
+        "Sirius",
+        "Canopus",
+        "Arcturus",
+        "Vega",
+        "Capella",
+        "Rigel",
+        "Procyon",
+        "Achernar",
+        "Betelgeuse",
+        "Altair"
+    };
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d. %s\n", i + 1, stars[i]);
+    }
+
+    return 0;
+}
