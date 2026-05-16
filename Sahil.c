a62973aef0bffafdbc903f1a2810cd9dcffcc249
Pujjalawar Sahil <pujjalwarsahil26@gmail.com>
2026-05-16 10:35:38 +0530

    added stars from sahil

---
+++ Sahil.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//print ten galaxy star names
+int main() {
+    const char *stars[] = {
+        "Sirius",
+        "Canopus",
+        "Arcturus",
+        "Vega",
+        "Capella",
+        "Rigel",
+        "Procyon",
+        "Achernar",
+        "Betelgeuse",
+        "Hadar"
+    };
+
+    for (int i = 0; i < 10; i++) {
+        printf("%s\n", stars[i]);
+    }
+
+    return 0;
+}`
