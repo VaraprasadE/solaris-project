a63b4edbcc6479415ad2599548399c30c7b16731
sandhya <sandhyapolisetty53@gmail.com>
2026-05-22 19:44:10 +0530

    added sstars.c file to print ten bright starts in the galaxy...

---
+++ sstars.c
@@ -0,0 +1,17 @@
+// print ten star names in the galaxy
+#include <stdio.h>
+int main(void)
+{
+    printf("Sirius\n");
+    printf("Canopus\n");
+    printf("Arcturus\n");
+    printf("Alpha Centauri\n");
+    printf("Vega\n");
+    printf("Capella\n");
+    printf("Rigel\n");
+    printf("Procyon\n");
+    printf("Achernar\n");
+    printf("Betelgeuse\n");
+
+    return 0;
+}
