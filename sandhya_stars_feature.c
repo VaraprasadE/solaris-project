9e302343718f86237018aa8fa9b58c008cd52902
sandhya <sandhyapolisetty53@gmail.com>
2026-05-26 10:29:01 +0530

    added the sandhya stars feature

---
+++ sandhya_stars_feature.c
@@ -0,0 +1,16 @@
+// print ten bright star names in the sky
+#include <stdio.h>
+int main() {
+    printf("Ten Bright Stars in the Sky:\n");
+    printf("1. Sirius\n");
+    printf("2. Canopus\n");
+    printf("3. Arcturus\n");
+    printf("4. Vega\n");
+    printf("5. Capella\n");
+    printf("6. Albireo\n");
+    printf("7. Altair\n");
+    printf("8. Betelgeuse\n");
+    printf("9. Rigel\n");
+    printf("10. Procyon\n");
+    return 0;
+}
